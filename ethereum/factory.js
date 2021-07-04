a285f994846e7e782b861e826ade42a7645005f0
MasonWong <84384337+masonwong2355@users.noreply.github.com>
2021-07-04 23:20:34 +0800

    redeploy

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -3,7 +3,7 @@ import web3 from './web3';
 
 const instance = new web3.eth.Contract(
     JSON.parse(CampaignFactroy.interface),
-    '0x105f395b3123A2b5786C89d0dD623A5631129459'
+    '0x61DdF8696aD6334f5939700a0A5591EDB608b421'
 );
 
 export default instance;
