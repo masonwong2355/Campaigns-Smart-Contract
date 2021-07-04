e7cb098f24bad84f080b06b8c5e2e3eb3286395f
MasonWong <84384337+masonwong2355@users.noreply.github.com>
2021-07-04 16:31:23 +0800

    adding export command

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -3,7 +3,7 @@ import web3 from './web3';
 
 const instance = new web3.eth.Contract(
     JSON.parse(CampaignFactroy.interface),
-    'endpoint'
+    '0x105f395b3123A2b5786C89d0dD623A5631129459'
 );
 
 export default instance;
